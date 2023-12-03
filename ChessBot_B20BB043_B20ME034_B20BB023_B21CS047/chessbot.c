ff00db854ed54271bc5d3b3699908eff142b55d8
tejpatange <75726328+tejpatange@users.noreply.github.com>
2023-12-03 20:44:29 +0530

    Update chessbot.c

--- ChessBot_B20BB043_B20ME034_B20BB023_B21CS047/chessbot.c
+++ ChessBot_B20BB043_B20ME034_B20BB023_B21CS047/chessbot.c
@@ -11,6 +11,15 @@ int main(){
     S_MOVELIST list[1];
 
     ParseFen(START_FEN, board);
+
+    printf("Welcome to the Game!\n");
+    printf("Instructions:\n");
+    printf("1. At the start Board will be printed with white to start.\n");
+    printf("2. Capital lettes are White and small letter are black.\n");
+    printf("3. P- Pawn, B-Bishop, N-Knight, R-Rook, Q-Queen, K-King.\n");
+    printf("4. If you want to enter move write current position of piece then where you want to move it\n");
+    printf(" ex:- if pawn is at e2 and want to move it to e4 write 'e2e5'\n");
+    printf("5. Have fun and enjoy the game!\n");
     
     char input[6];
     int Move = NOMOVE;
